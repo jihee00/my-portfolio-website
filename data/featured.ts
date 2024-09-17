911d51fa752b5ff2ddc036731661c1341585c0de
jihee Kim <jkim541@myseneca.ca>
2024-09-17 13:55:39 -0400

    create card header

---
+++ data/featured.ts
@@ -0,0 +1,38 @@
+import { video } from "framer-motion/client";
+
+const featuredData = [
+    {
+        title: "Featured",
+        logo: "",
+        tag: "Feb 2024",
+        video: "https://cdn.dribbble.com/userupload/3743863/file/original-ed64ce215ecf5f86ead7fedaca82c84c.mp4",
+        credits: "Julia Dmitrievna",
+        link: "https://dribbble.com/shots/19587956-Okarys-Brand-Identity",
+    },
+    {
+      title: "Featured",
+      logo: "",
+      tag: "Feb 2024",
+      video: "https://cdn.dribbble.com/userupload/3743863/file/original-ed64ce215ecf5f86ead7fedaca82c84c.mp4",
+      credits: "Julia Dmitrievna",
+      link: "https://dribbble.com/shots/19587956-Okarys-Brand-Identity",
+  },
+  {
+    title: "Featured",
+    logo: "",
+    tag: "Feb 2024",
+    video: "https://cdn.dribbble.com/userupload/3743863/file/original-ed64ce215ecf5f86ead7fedaca82c84c.mp4",
+    credits: "Julia Dmitrievna",
+    link: "https://dribbble.com/shots/19587956-Okarys-Brand-Identity",
+},
+{
+  title: "Featured",
+  logo: "",
+  tag: "Feb 2024",
+  video: "https://cdn.dribbble.com/userupload/3743863/file/original-ed64ce215ecf5f86ead7fedaca82c84c.mp4",
+  credits: "Julia Dmitrievna",
+  link: "https://dribbble.com/shots/19587956-Okarys-Brand-Identity",
+},
+];
+
+export default featuredData;
--- sections/featured.tsx
+++ sections/featured.tsx
@@ -1,10 +1,21 @@
 import Heading from "@/components/heading/heading";
+import FeaturedCard from "@/components/cards/featured/featured-card";
+import { featuredData } from "@/data";
+
+const MainFeatured = featuredData[0];
 
 export default function FeaturedSection() {
   return (
     <div className="pt-24 px-3 lg:px-8">
       {/*Heading*/}
       <Heading number="01" title_1="Featured" title_2="Work" />
+      {/*Main Featured Card*/}
+      <FeaturedCard 
+      active
+      title={MainFeatured.title} 
+      tag={MainFeatured.tag}
+      video={MainFeatured.video}
+      />
     </div>
   );
 }
