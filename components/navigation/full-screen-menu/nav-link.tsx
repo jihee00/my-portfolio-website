79cb2a6562e853e8dcf27de2f8e3785ed42d3136
jihee Kim <jkim541@myseneca.ca>
2025-01-02 16:40:09 -0500

    create dot effect with framer motion

--- components/navigation/full-screen-menu/nav-link.tsx
+++ components/navigation/full-screen-menu/nav-link.tsx
@@ -1,4 +1,4 @@
-import { FC } from "react"
+import { FC, useState } from "react"
 import { motion } from 'framer-motion'
 import { slide } from "./animation"
 import Link from "next/link"
@@ -14,12 +14,30 @@ interface NavLinkProps {
 
 const NavLink: FC<NavLinkProps> = ({ data }) => {
   const { title, href, index } = data;
-  return <motion.div className="relative flex items-center" variants={slide} custom={index} initial="initial" animate="enter" exit="exit">
-    <motion.div variants={scale} className="hidden"></motion.div>
-    <Link href={href} className="text-[6vw] uppercase leading-[96%] font-bold">
+  const [hovered, setHovered] = useState<boolean>(false);
+  return (
+    <motion.div 
+      className="relative flex items-center z-40" 
+      variants={slide} 
+      custom={index} 
+      initial="initial" 
+      animate="enter" 
+      exit="exit"
+      onMouseEnter={() => setHovered(true)}
+      onMouseLeave={() => setHovered(false)}
+    >
+
+    <motion.div 
+      variants={scale}
+      animate={hovered ? "open" : "closed"}
+      className="w-2.5 h-2.5 bg-white rounded-full absolute -left-[30px]"></motion.div>
+    <Link 
+      href={href} 
+      className="text-[6vw] uppercase leading-[96%] font-bold text-primary-foreground">
     {title}
     </Link>
-  </motion.div>;
+  </motion.div>
+  );
 };
 
 export default NavLink;
