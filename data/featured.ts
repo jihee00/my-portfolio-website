97d356b1961d79e6eb3e6d2257b5a1d2a3b65a5e
jihee Kim <jkim541@myseneca.ca>
2025-04-03 16:23:53 -0400

    update featured

--- data/featured.ts
+++ data/featured.ts
@@ -1,11 +1,27 @@
 const featuredData = [
   {
+    title: "WordPress Development",
+    logo: "Corporate Website",
+    tag: "Apr 2025",
+    video: "https://youtu.be/QddjDntQtFc", 
+    credits: "",
+    link: "https://urbctrading.com/",
+  },
+  {
       title: "Web Development",
       logo: "Enrollment App",
       tag: "Aug 2024",
       video: "https://youtu.be/uaE7z965lqU", 
       credits: "",
       link: "https://github.com/jihee00/enrollment-app",
+  },
+  {
+    title: "Android App Development",
+    logo: "",
+    tag: "Jul 2024",
+    video: "https://youtu.be/3WCVrLUbY_4",
+    credits: "",
+    link: "https://youtu.be/3WCVrLUbY_4",
   },
   {
       title: "Web Development",
@@ -18,26 +34,18 @@ const featuredData = [
   {
     title: "iOS App Development",
     logo: "",
+    tag: "Mar 2024",
+    video: "https://youtu.be/CRQBPcrB6Uw", 
+    credits: "",
+    link: "https://youtu.be/CRQBPcrB6Uw",
+  },
+  {
+    title: "iOS App Development",
+    logo: "",
     tag: "Feb 2024",
     video: "https://youtu.be/vahhNaPcP_g", 
     credits: "",
     link: "https://youtu.be/vahhNaPcP_g",
-  },
-  {
-      title: "iOS App Development",
-      logo: "",
-      tag: "Mar 2024",
-      video: "https://youtu.be/CRQBPcrB6Uw", 
-      credits: "",
-      link: "https://youtu.be/CRQBPcrB6Uw",
-  },
-  {
-      title: "Android App Development",
-      logo: "",
-      tag: "Jul 2024",
-      video: "https://youtu.be/3WCVrLUbY_4",
-      credits: "",
-      link: "https://youtu.be/3WCVrLUbY_4",
   },
 ];
 
